7113bf90205f34fdc6d53118878803b8173eea15
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-05 14:33:33 +0400

    Adding AppointmentRepo with GetAppointmentsByClinc and GetAppointmentsByPatient methods.

---
+++ Repositories/AppointmentRepo.cs
@@ -0,0 +1,27 @@
+﻿using HospitalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.Repositories
+{
+    public class AppointmentRepo : IAppointmentRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppointmentRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //view appoinment by clinic id
+        public Appoinment GetAppointmentsByClinci(int cid)
+        {
+            return _context.Appoinments.FirstOrDefault(a => a.CID == cid);
+        }
+
+        //view appoinment by patient id 
+        public Appoinment GetAppointmentsByPatient(int pid)
+        {
+            return _context.Appoinments.FirstOrDefault(a => a.PID == pid);
+        }
+    }
+}
