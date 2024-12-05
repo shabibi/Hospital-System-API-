b2647c63f35cce57f7a4423f0ff676bfaf899e05
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-05 14:45:39 +0400

    Adding ClinicService and IClinicService

---
+++ Services/ClinicService.cs
@@ -0,0 +1,24 @@
+﻿using HospitalSystem.Models;
+using HospitalSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.Services
+{
+    public class ClinicService : IClinicService
+    {
+        private readonly ClinicRepo _clinicRepo;
+
+        public ClinicService(ClinicRepo clinicRepo)
+        {
+            _clinicRepo = clinicRepo;
+        }
+        public void AddClinic(Clinic clinic)
+        {
+            _clinicRepo.AddClinic(clinic);
+        }
+        public IEnumerable<Clinic> GetAllClinic()
+        {
+            return _clinicRepo.GetAllClinic().ToList();
+        }
+    }
+}
---
+++ Services/IClinicService.cs
@@ -0,0 +1,10 @@
+﻿using HospitalSystem.Models;
+
+namespace HospitalSystem.Services
+{
+    public interface IClinicService
+    {
+        void AddClinic(Clinic clinic);
+        IEnumerable<Clinic> GetAllClinic();
+    }
+}
