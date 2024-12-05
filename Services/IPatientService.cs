5eb1da9defa807d92c58cf973e81e90d29d49319
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-05 14:42:28 +0400

    Adding PatientService and IPatientService in service file

---
+++ Services/IPatientService.cs
@@ -0,0 +1,10 @@
+﻿using HospitalSystem.Models;
+
+namespace HospitalSystem.Services
+{
+    public interface IPatientService
+    {
+        void AddPatient(Patient patient);
+        IEnumerable<Patient> GetAllPatients();
+    }
+}
---
+++ Services/PatientService.cs
@@ -0,0 +1,24 @@
+﻿using HospitalSystem.Models;
+using HospitalSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly IPatientRepo _patientRepo;
+        public PatientService(IPatientRepo patientRepo)
+        {
+            _patientRepo = patientRepo;
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            _patientRepo.AddPatient(patient);
+        }
+        public IEnumerable<Patient> GetAllPatients()
+        {
+            return _patientRepo.GetAllPatients().ToList();
+        }
+    }
+}
