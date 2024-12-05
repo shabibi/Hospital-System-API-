af06c09c3b4c5cf2a673c864889c2f5ea0416ceb
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-05 14:09:01 +0400

    Adding ClinicRepo with AddClinic and GetAllClinic methods.

---
+++ Repositories/ClinicRepo.cs
@@ -0,0 +1,28 @@
+﻿using HospitalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.Repositories
+{
+    public class ClinicRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public ClinicRepo (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //Add clinic
+        public void AddClinic( Clinic clinic )
+        {
+            _context.Clinic.Add( clinic );
+            _context.SaveChanges();
+        }
+
+        //view all clinics
+        public IEnumerable<Clinic> GetAllClinic()
+        {
+            return _context.Clinic.Include(c => c.Appoinments).ToList();
+        }
+    }
+}
