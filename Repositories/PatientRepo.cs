abae36ca447117fcf7c067f3b7235f831dc26b2f
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-05 14:18:09 +0400

    Adding PatientRepo with AddPatient and GetAllPatients methods

---
+++ Repositories/PatientRepo.cs
@@ -0,0 +1,28 @@
+﻿using HospitalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.Repositories
+{
+    public class PatientRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientRepo (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Add patirnt 
+        public void AddPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+        }
+
+        //Get All Patients
+        public IEnumerable<Patient> GetAllPatients()
+        {
+            return _context.Patients.Include(p => p.Appoinments).ToList();
+        }
+    }
+}
