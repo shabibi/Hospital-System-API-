a31ccca9ee466b04a4b8c9f2a4e18404535ad90a
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-08 12:25:38 +0400

    Adding ClinicController

---
+++ Controllers/ClinicController.cs
@@ -0,0 +1,63 @@
+﻿using HospitalSystem.Models;
+using HospitalSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ClinicController : ControllerBase
+    {
+        private readonly IClinicService _clinicService;
+
+        public ClinicController(IClinicService clinicService)
+        {
+            _clinicService = clinicService;
+        }
+
+
+        [HttpPost]
+        public IActionResult AddClinic(ClinicDTO clinicDTO)
+        {
+            try
+            {
+                if (clinicDTO == null)
+                {
+                    return BadRequest("Clinic data is null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var clinic = new Clinic
+                {
+                    specilization = clinicDTO.Specilization,
+                    NumberOfSlots = clinicDTO.NumberOfSlots,
+                };
+                _clinicService.AddClinic(clinic);
+                return CreatedAtAction(nameof(AddClinic), new { id = clinic.CID }, clinic);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                return StatusCode(500, "Error ");
+            }
+
+        }
+
+        [HttpGet("GetAllClincs")]
+        public IActionResult GetAllPatients()
+        {
+            try
+            {
+                var clinics = _clinicService.GetAllClinic();
+                return Ok(clinics);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+    }
+}
